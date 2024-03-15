db84c50150737c3d9e4396a6818f2bf67c190da6
mamadou <amadou.mbodj@ynov.com>
2024-03-15 12:12:51 +0100

    interface 10%

--- Interface.py
+++ Interface.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import Jeu
 
 class Interface:
     def __init__(self):
@@ -8,27 +8,18 @@ class Interface:
         self.cnv.grid()
         self.taille_carre = 100
 
-        self.ids = []
-
         self.creer_carres()
-
-        self.lier_evenements()
 
     def creer_carres(self):
         for i in range(7):
             for j in range(6):
-
                 id = self.cnv.create_rectangle(i * self.taille_carre, j * self.taille_carre,
                                                (i + 1) * self.taille_carre, (j + 1) * self.taille_carre, fill="white")
-                self.ids.append(id)
+                self.cnv.tag_bind(id, "<Button-1>", self.change_color)
 
     def change_color(self, event):
         id = self.cnv.find_withtag("current")[0]
         self.cnv.itemconfig(id, fill="red")
-
-    def lier_evenements(self):
-        for id in self.ids:
-            self.cnv.tag_bind(id, "<Button-1>", self.change_color)
 
     def run(self):
         self.fen.mainloop()
