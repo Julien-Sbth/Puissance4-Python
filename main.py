30a4c4f515285f962565f1023ad2b61a1428d010
JS <131463436+Julien-Sbth@users.noreply.github.com>
2024-03-16 10:54:23 +0100

    [Update Files For IA Upgrade

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import csv
-import random as rand
-
-import numpy as np
 import keyboard
 
 from IA import IA
@@ -9,31 +5,31 @@ from Jeu import Puissance4
 
 
 def main():
-    while True:  # Boucle infinie
+    while True:
         jeu = Puissance4()
-        ia1 = IA()  # Première instance de l'IA
-        ia2 = IA()  # Première instance de l'IA
-        ia1.charger_donnees()  # Charger les données pour la première IA
+        ia1 = IA()
+        ia2 = IA()
+        ia1.charger_donnees()
         ia2.charger_donnees()
         print(jeu.plateau)
         while True:
             coup_ia1 = ia1.choisir_coup(jeu.plateau)
-            if coup_ia1 is not None:  # Vérifier si un coup est disponible
+            if coup_ia1 is not None:
                 jeu.jouer(coup_ia1)
-                ia1.coups_joues.append(coup_ia1)  # Ajouter le coup joué par l'IA 2 à ses coups joués
+                ia1.coups_joues.append(coup_ia1)
                 if jeu.check_victoire():
-                    ia1.enregistrer_resultat(ia1.coups_joues)  # Enregistrer les coups joués par l'IA 2
+                    ia1.enregistrer_resultat(ia1.coups_joues)
                     break
             else:
                 print("Aucun coup disponible pour l'IA 2.")
                 break
 
-            coup_ia2 = ia2.choisir_coup(jeu.plateau)
-            if coup_ia2 is not None:  # Vérifier si un coup est disponible
-                jeu.jouer(coup_ia2)
-                ia2.coups_joues.append(coup_ia2)  # Ajouter le coup joué par l'IA 2 à ses coups joués
+            coup_ia = ia1.choisir_coup(jeu.plateau)
+            if coup_ia is not None:
+                jeu.jouer(coup_ia)
+                ia2.coups_joues.append(coup_ia)
                 if jeu.check_victoire():
-                    ia2.enregistrer_resultat(ia2.coups_joues)  # Enregistrer les coups joués par l'IA 2
+                    ia1.enregistrer_resultat(ia2.coups_joues)
                     break
             else:
                 print("Aucun coup disponible pour l'IA 2.")
